d4efcbb9d702ae046c6c0c480cd88f7f3fa8a15d
jhernandezga <jhernandezga@unal.edu.co>
2020-05-24 14:32:42 -0500

    Inicio

---
+++ src/datos/Contacto.java
@@ -0,0 +1,49 @@
+package datos;
+
+public class Contacto {
+	
+	private String nombre;
+	private String apellido;
+	private  String email;
+	private String direccion;
+	private String ciudad;
+	private long telefono;
+	
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getApellido() {
+		return apellido;
+	}
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getDireccion() {
+		return direccion;
+	}
+	public void setDireccion(String direccion) {
+		this.direccion = direccion;
+	}
+	public String getCiudad() {
+		return ciudad;
+	}
+	public void setCiudad(String ciudad) {
+		this.ciudad = ciudad;
+	}
+	public long getTelefono() {
+		return telefono;
+	}
+	public void setTelefono(long telefono) {
+		this.telefono = telefono;
+	}
+	
+}
